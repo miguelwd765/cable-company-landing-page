384405f437b6dcfabaad9e7a708811b1bf279e55
Miguel Pacheco <miguelwebdev765@gmail.com>
2021-12-21 09:15:48 -0600

    create mobile version of nav bar. set custom variables in CSS

---
+++ app.js
@@ -0,0 +1,13 @@
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function (e) {
+  const linksHeight = links.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
